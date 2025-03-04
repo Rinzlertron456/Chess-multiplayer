e0136656636e00ab836ce62925a538d82c2f62a3
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-03-04 19:47:02 +0530

    rollback-deployment-changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+// import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  root: path.join(__dirname, "src"), // Update this path if your index.html is in a different directory
+  // root: path.join(__dirname, "src"), // Update this path if your index.html is in a different directory
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "./index.html"), // Update this path if your index.html is in a different directory
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: path.resolve(__dirname, "./index.html"), // Update this path if your index.html is in a different directory
+  //     },
+  //   },
+  // },
 });
