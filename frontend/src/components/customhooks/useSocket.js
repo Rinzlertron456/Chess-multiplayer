e136c57397a66a21af2a3a810b964e7365fa0b24
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-03-26 12:23:27 +0530

    render-url-update

--- frontend/src/components/customhooks/useSocket.js
+++ frontend/src/components/customhooks/useSocket.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const URL = "ws://localhost:8085";
+const URL = "wss://chess-multiplayer-bbvr.onrender.com";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState(null);
