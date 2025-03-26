5183b4df27595d60ca63cefe2cb1d9168c90bad9
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-03-26 18:21:53 +0530

    useSocket fix

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,6 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const GameManager_1 = require("./GameManager");
+// const cors = require("cors");
+// const app = require("express");
+// app.use(
+//   cors({
+//     origin: "https://chess-multiplayer-three.vercel.app/",
+//   })
+// );
 const wss = new ws_1.WebSocketServer({ port: 8085 });
 const gameManager = new GameManager_1.GameManager();
 wss.on("connection", function connection(ws) {
--- frontend/src/components/customhooks/useSocket.js
+++ frontend/src/components/customhooks/useSocket.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const URL = "wss://chess-multiplayer-bbvr.onrender.com";
+const URL = "wss://chess-multiplayer-c7t4.onrender.com";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState(null);
