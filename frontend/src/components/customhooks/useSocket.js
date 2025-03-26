c9a9746b2f5eb6d0bdd61e07c76ff54ad755e65c
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-03-26 20:12:39 +0530

    websocket url port insert

--- frontend/src/components/customhooks/useSocket.js
+++ frontend/src/components/customhooks/useSocket.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const URL = "wss://chess-multiplayer-c7t4.onrender.com";
+const URL = "wss://chess-multiplayer-c7t4.onrender.com:8085";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState(null);
