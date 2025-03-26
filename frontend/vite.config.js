57a30a63c99668909e1e08c382a032d79988886d
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-03-26 22:41:39 +0530

    replaces json files config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
-  // root: path.join(__dirname, "src"), // Update this path if your index.html is in a different directory
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ["chess.js"],
-    },
-  },
-  optimizeDeps: {
-    include: ["chess.js"],
-  },
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: path.resolve(__dirname, "./index.html"), // Update this path if your index.html is in a different directory
-  //     },
-  //   },
-  // },
 });
